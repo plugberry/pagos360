ae19db654368d6dc8bb5386ab530c098a98e86a8
Martin Quinteros <maq@adhoc.com.ar>
2025-06-02 19:47:17 +0000

    [IMP] Minor changes before prod
    X-original-commit: 01a5ceed77368b0956afaff79faf30bc87ce217a

---
+++ barcode_invoice_pagos360/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Barcode Invoice Pagos360',
+    'version': '17.0.1.1.0',
+    'summary': 'Add a pagos360 payment barcode into the invoice',
+    'description': '',
+    'category': '',
+    'author': '',
+    'maintainer': '',
+    'website': '',
+    'license': 'LGPL-3',
+    'contributors': [
+        '',
+    ],
+    'depends': [
+        'payment_pagos360',
+    ],
+    'data': [
+        'views/report_invoice.xml',
+    ],
+    'installable': True,
+    'application': False
+}
