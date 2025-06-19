9dbac1c43501f24d1ff3e72ad8aa4ec4bcb50b81
Alexis Lopez <loa@adhoc.com.ar>
2025-06-19 22:02:20 +0000

    [ADD] barcode_invoice_pagos360: add barcode in invoice when posted on background
    closes plugberry/pagos360#70
    
    Signed-off-by: augusto-weiss <awe@adhoc.com.ar>

--- barcode_invoice_pagos360/models/account_move.py
+++ barcode_invoice_pagos360/models/account_move.py
@@ -14,7 +14,7 @@ class AccountMove(models.Model):
     pagos360_barcode = fields.Char(copy=False)
     pagos360_barcode_image = fields.Binary(copy=False, attachment=True)
     pagos360_rp_barcode = fields.Char(copy=False)
-    pagos360_rp_barcode_image = fields.Char(copy=False)
+    pagos360_rp_barcode_image = fields.Binary(copy=False, attachment=True)
 
     def _payment_barcode_request_pagos360(self):
         """Request barcode from Pagos360 for the given invoices.
@@ -33,10 +33,7 @@ class AccountMove(models.Model):
                 "payer_name": invoice.partner_id.name,
                 "external_reference": f"inv-{provider_id.id}-{invoice.id}",
             }}
-            _logger.info(
-                "Sending '/payment-request' request for link creation:\n%s",
-                pprint.pformat(payload),
-            )
+
             payment_data = provider_id._pagos360_make_request(
                 "/payment-request", data=payload
             )
@@ -45,14 +42,17 @@ class AccountMove(models.Model):
                 invoice.pagos360_barcode = payment_data.get("barcode")
                 svg = requests.get(payment_data["barcode_url"]).content
                 invoice.pagos360_barcode_image = base64.b64encode(svg)
-            if payment_data.get("rapipago_barcode"):
+            if payment_data.get("rapipago_barcode") and payment_data.get("rapipago_barcode") != payment_data.get("barcode"):
                 invoice.pagos360_rp_barcode = payment_data.get("rapipago_barcode")
                 rp_svg = requests.get(payment_data["rapipago_barcode_url"]).content
                 invoice.pagos360_rp_barcode_image = base64.b64encode(rp_svg)
 
     def action_post(self):
         res = super().action_post()
+        self._create_pagos360_barcode()
+        return res
+
+    def _create_pagos360_barcode(self):
         invoices_to_process = self.filtered(lambda inv: inv.move_type == 'out_invoice' and inv.state == 'posted' and inv.payment_state in ['not_paid', 'partial'] and not inv.pagos360_barcode)
         if invoices_to_process:
             invoices_to_process._payment_barcode_request_pagos360()
-        return res
