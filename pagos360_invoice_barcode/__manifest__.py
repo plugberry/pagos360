344bd09ec76a5f78662de5dfb0f6eb61d4bfe45b
Alexis Lopez <loa@adhoc.com.ar>
2025-07-23 09:09:34 -0300

    [FW] pagos360_invoice_barcode: fw to 18

---
+++ pagos360_invoice_barcode/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Pagos360 Invoice Barcode",
+    "version": "18.0.1.0.0",
+    "summary": "Add a pagos360 payment barcode into the invoice",
+    "category": "",
+    "author": "Plugberry",
+    "maintainer": "",
+    "website": "",
+    "license": "LGPL-3",
+    "contributors": [
+        "",
+    ],
+    "depends": [
+        "payment_pagos360",
+    ],
+    "data": [
+        "views/report_invoice.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
