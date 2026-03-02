d541e03e5ae87acae3b6afc6f3920371381ea425
roboadhoc <roboadhoc@adhoc.com.ar>
2026-03-02 14:29:22 +0000

    [BOT] Bump version: payment_pagos360 18.0.1.1.0
    Merged: plugberry/pagos360#90

--- payment_pagos360/__manifest__.py
+++ payment_pagos360/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Payment Provider: Pagos 360",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.0",
     "category": "Accounting/Payment Providers",
     "sequence": 350,
     "author": "Plugberry",
